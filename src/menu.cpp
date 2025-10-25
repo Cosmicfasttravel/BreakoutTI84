4a71f5b0db971efac0fcf314d43244aa0f963d86
Marcus <marcus.chumanov@gmail.com>
2025-10-24 21:58:05 -0500

    Added framework for options menu

--- src/menu.cpp
+++ src/menu.cpp
@@ -1,6 +1,7 @@
 ﻿#include "menu.h"
 #include <keypadc.h>
 #include "../../include/graphx.h"
+#include "../../include/ti/getcsc.h"
 
 int main_menu() {
     menuOption = MENU_START;
@@ -13,26 +14,63 @@ int main_menu() {
             gfx_PrintStringXY("_______________", 10, 22);
             gfx_SetTextScale(1, 1);
             gfx_PrintStringXY((menuOption == MENU_START) ? "-> Start" : "   Start", 60, 100);
-            gfx_PrintStringXY((menuOption == MENU_EXIT) ? "-> Exit" : "   Exit", 60, 120);
+            gfx_PrintStringXY((menuOption == MENU_OPTIONS) ? "-> Options" : "   Options", 60, 120);
+            gfx_PrintStringXY((menuOption == MENU_EXIT) ? "-> Exit" : "   Exit", 60, 140);
             gfx_SwapDraw();
 
             // Get input
             sk_key_t key;
-            while (!(key = os_GetCSC()));
+            while (!((key = os_GetCSC()))) {}
             switch (key) {
                 case sk_Up:
-                    menuOption = static_cast<MenuOption>((menuOption - 1) % 2);
+                    menuOption = static_cast<MenuOption>((menuOption - 1 + 3) % 3);
                     break;
                 case sk_Down:
-                    menuOption = static_cast<MenuOption>((menuOption + 1) % 2);
+                    menuOption = static_cast<MenuOption>(abs((menuOption + 1) % 3));
                     break;
                 case sk_Enter:
-                    if (menuOption == MENU_EXIT) {
-                        return 0;  // Exit game
+                    if (menuOption == MENU_EXIT) return 0;
+                    if (menuOption == MENU_OPTIONS) {
+                        options_menu();
+                        menuOption = MENU_OPTIONS;
                     }
-                    return 1;  // Start game
+                    if (menuOption == MENU_START) return 1;  // Start game
                 default:
                     break;  // Ignore other keys
             }
         }
     }
+
+int options_menu() {
+    while (true) {
+        gfx_FillScreen(255);
+        gfx_SetTextFGColor(0);
+        gfx_SetTextScale(2, 2);
+        gfx_PrintStringXY("Options", 10, 20);
+        gfx_PrintStringXY("_______________", 10, 22);
+        gfx_SetTextScale(1, 1);
+        gfx_PrintStringXY((menuOption == OPTIONS_1) ? "-> Option 1" : "   Option 1", 60, 100); // replace options with actual options
+        gfx_PrintStringXY((menuOption == OPTIONS_2) ? "-> Option 2" : "   Option 2", 60, 120);
+        gfx_PrintStringXY((menuOption == OPTIONS_3) ? "-> Option 3" : "   Option 3", 60, 140);
+        gfx_SetTextScale(1, 1);
+        gfx_PrintStringXY("2nd to quit", 235, 229);
+        gfx_Rectangle(230, 225, 80, 15);
+        gfx_SwapDraw();
+        sk_key_t key;
+        while (!((key = os_GetCSC()))) {}
+        switch (key) {
+            case sk_Up:
+                menuOption = static_cast<MenuOption>((menuOption - 1 + 3) % 3);
+                break;
+            case sk_Down:
+                menuOption = static_cast<MenuOption>(abs((menuOption + 1) % 3));
+                break;
+            case sk_Enter:
+                break;
+            case sk_2nd:
+                return 0;
+            default:
+                break;  // Ignore other keys
+        }
+    }
+}
--- src/menu.h
+++ src/menu.h
@@ -3,11 +3,16 @@
 #include <ti/getcsc.h>
 enum MenuOption {
     MENU_START,
+    MENU_OPTIONS,
     MENU_EXIT,
-
+};
+enum Options {
+    OPTIONS_1,
+    OPTIONS_2,
+    OPTIONS_3,
 };
 
 static enum MenuOption menuOption;
 int main_menu();
-void options_menu();
+int options_menu();
 #endif //MENU_H
